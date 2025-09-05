f5a7d343c379573b965ac08e412101e2de1c9f66
Komi Golova <komi.golov@jetbrains.com>
2025-09-05 12:17:16 +0200

    Add exercises for the tutorial

--- FunctionalProgramming/Tutorial01.lean
+++ FunctionalProgramming/Tutorial01.lean
@@ -1,2 +1,57 @@
 import LeanSearchClient.Basic
 import Mathlib.Tactic.Linarith
+
+-- The following exercises are from https://leanprover.github.io/theorem_proving_in_lean4/Propositions-and-Proofs/#propositions-and-proofs
+
+namespace Propositional
+variable (p q r : Prop)
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := sorry
+example : p ∨ q ↔ q ∨ p := sorry
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+end Propositional
+
+-- The following exercises are from https://leanprover.github.io/theorem_proving_in_lean4/Quantifiers-and-Equality/#quantifiers-and-equality
+
+namespace Predicate
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+
+example : α → ((∀ x : α, r) ↔ r) := sorry
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
+end Predicate
+
+-- Let's define `and` and `or` functions ourselves and
+-- show that they behave as expected.
+
+def myAnd : Bool -> Bool -> Bool := sorry
+def myOr : Bool -> Bool -> Bool := sorry
+
+example (b1 b2 : Bool) : myAnd b1 b2 = true ↔ b1 = true ∧ b2 = true := sorry
+example (b1 b2 : Bool) : myOr b1 b2 = true ↔ b1 = true ∨ b2 = true := sorry
