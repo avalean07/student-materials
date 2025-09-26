2029d8fe859832daa382c9f3ef53b058d7853af4
avalean07 <avalean@constructor.university>
2025-09-26 12:03:59 +0200

    tutorial 2

--- FunctionalProgramming/Tutorial02.lean
+++ FunctionalProgramming/Tutorial02.lean
@@ -6,14 +6,36 @@ namespace Week02
 -- In each case, one statement is easy to prove, while the other requires induction.
 -- Try to look at the definition of the operations and see whether you can
 -- figure out why that is:
+
+--use reduce at, efl, indcution, exact
 #print Nat.add
 #print Nat.mul
 
-example (n : ℕ) : n + 0 = n := sorry
-example (n : ℕ) : 0 + n = n := sorry
-example (n : ℕ) : n * 0 = 0 := sorry
-example (n : ℕ) : 0 * n = 0 := sorry
-example (n : ℕ) : n * 1 = n := sorry
+example (n : ℕ) : n + 0 = n := by
+rw [add_zero]
+
+example (n : ℕ) : 0 + n = n := by
+induction n with
+| zero =>
+  rw [add_zero]
+| succ k ih =>
+  rw [Nat.add_succ, ih]
+
+example (n : ℕ) : n * 0 = 0 := by
+rw [mul_zero]
+
+example (n : ℕ) : 0 * n = 0 := by
+induction n with
+| zero =>
+  rw [mul_zero]
+| succ k ih =>
+  rw [Nat.mul_succ, ih, Nat.add_zero]
+
+
+
+example (n : ℕ) : n * 1 = n := by
+rw [mul_one]
+
 example (n : ℕ) : 1 * n = n := sorry
 
 example (n m : Nat) : n + m = m + n := sorry
